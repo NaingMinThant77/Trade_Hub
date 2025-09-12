b85fd39baccbaf8761c77543ce786338ac1e8217
NaingMinThant77 <naingminthant07@gmail.com>
2025-09-12 21:55:32 +0630

    UI_Update

--- Client/src/pages/HomePage/Details.jsx
+++ Client/src/pages/HomePage/Details.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { getProductsById } from "../../apicalls/public";
-import TradePoint from "../../images/TradePoint1.png"
+import TradePoint from "../../images/TradePoint2.png"
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 
 import { Form, Input, message } from "antd";
